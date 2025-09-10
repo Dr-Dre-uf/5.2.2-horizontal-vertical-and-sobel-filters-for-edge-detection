8f9fd96fff5e09c1312f130520f35bc106bc3519
Dr-Dre-uf <deandrecoleman@ufl.edu>
2025-09-10 14:41:57 +0000

    filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,15 +31,27 @@ filter_type = st.sidebar.radio(
     ("Horizontal", "Vertical", "Sobel")
 )
 
+# Filter size slider
+filter_size = st.sidebar.slider(
+    "Filter Size:",
+    min_value=3,
+    max_value=11,
+    value=3,
+    step=2
+)
+
 # Define custom horizontal edge filter
-horiz_filter = np.array([[1, 1, 1],
-                          [0, 0, 0],
-                          [-1, -1, -1]], dtype=np.float32)
+kernel_size = filter_size
+horiz_filter = np.zeros((kernel_size, kernel_size), dtype=np.float32)
+horiz_filter[kernel_size // 2, :] = np.ones(kernel_size)  # Dynamic coefficient creation
+horiz_filter[kernel_size // 2 - 1, :] = 0
+horiz_filter[kernel_size // 2 + 1, :] = -np.ones(kernel_size)  # Dynamic coefficient creation
 
 # Define custom vertical edge filter
-vert_filter = np.array([[1, 0, -1],
-                          [1, 0, -1],
-                          [1, 0, -1]], dtype=np.float32)
+vert_filter = np.zeros((kernel_size, kernel_size), dtype=np.float32)
+vert_filter[:, kernel_size // 2] = np.ones(kernel_size)  # Dynamic coefficient creation
+vert_filter[:, kernel_size // 2 - 1] = 0
+vert_filter[:, kernel_size // 2 + 1] = -np.ones(kernel_size) # Dynamic coefficient creation
 
 # Apply filters
 if filter_type == "Horizontal":
@@ -47,8 +59,8 @@ if filter_type == "Horizontal":
 elif filter_type == "Vertical":
     filtered_image = cv2.filter2D(img, -1, vert_filter)
 elif filter_type == "Sobel":
-    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
-    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=filter_size)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=filter_size)
     filtered_image = np.uint8(np.absolute(sobelx + sobely))
 
 # Convert to uint8 before displaying
@@ -59,4 +71,4 @@ st.subheader("Original Image")
 st.image(img, caption=f"Original {image_source}")
 
 st.subheader("Filtered Image")
-st.image(filtered_image, caption=f"{filter_type} Filtered {image_source}")
+st.image(filtered_image, caption=f"{filter_type} Filtered {image_source} (Size: {filter_size})")
